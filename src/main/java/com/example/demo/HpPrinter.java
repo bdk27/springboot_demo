9109648aad6a034f72184fdb5de75b64b6872249
YCheng <briansam195@gmail.com>
2025-04-25 15:10:57 +0800

    設定application.properties和使用@Value

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer{
 
     private int count;
+    @Value("${my.name:sam}")
+    private String name;
 
     @PostConstruct
     public void init() {
@@ -18,5 +21,6 @@ public class HpPrinter implements Printer{
         count--;
         System.out.println("Hp印表機: " + message);
         System.out.println("剩餘使用次數: " + count);
+        System.out.println("使用者名稱: " + name);
     }
 }
