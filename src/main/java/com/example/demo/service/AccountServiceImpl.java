aa8d22cb44b93175e8f0d834b8935f6a1cfad726
YCheng <briansam195@gmail.com>
2025-05-23 11:37:14 +0800

    新增交易管理Transactional

--- src/main/java/com/example/demo/service/AccountServiceImpl.java
+++ src/main/java/com/example/demo/service/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class AccountServiceImpl implements AccountService{
@@ -10,9 +11,11 @@ public class AccountServiceImpl implements AccountService{
     @Autowired
     private AccountDao accountDao;
 
+    @Transactional
     @Override
     public void transfer(int fromAccountId, int toAccountId, int money) {
         accountDao.decreaseMoney(fromAccountId, money);
+
         accountDao.addMoney(toAccountId, money);
     }
 }
